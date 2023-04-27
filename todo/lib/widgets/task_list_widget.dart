5d826f92f64582655be6bfc5e05667bfd31b4f39
xul <54ulxd@gmail.com>
2023-04-27 00:36:54 -0600

    Basic http tasks get implement

--- todo/lib/widgets/task_list_widget.dart
+++ todo/lib/widgets/task_list_widget.dart
@@ -1,7 +1,5 @@
 
 import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:todo/widgets/task_item_widget.dart';
 import 'package:http/http.dart' as http;
@@ -10,8 +8,7 @@ import '../models/Todo.dart';
 final String token = "e864a0c9eda63181d7d65bc73e61e3dc6b74ef9b82f7049f1fc7d9fc8f29706025bd271d1ee1822b15d654a84e1a0997b973a46f923cc9977b3fcbb064179ecd";
 
 Future<List<Todo>> fetchTodos() async {
-  var url = Uri.parse('https://ecsdevapi.nextline.mx/vdev/tasks-challenge/tasks');
-
+  var url = Uri.parse('https://ecsdevapi.nextline.mx/vdev/tasks-challenge/tasks?token=0xFF');
   // create a new HTTP GET request
   var request = http.Request('GET', url);
 
@@ -21,19 +18,24 @@ Future<List<Todo>> fetchTodos() async {
     'Authorization': 'Bearer ${token}'
   });
 
+  
+
   // send the request and wait for the response
   final response = await request.send();
- 
+
   if (response.statusCode == 200) {
     String responseBody = await response.stream.bytesToString();
     final jsonData = jsonDecode(responseBody) as List<dynamic>;
+    print(jsonData);
     return jsonData.map((todoJson) {
       return Todo(
         title: todoJson['title'] as String,
-        isCompleted: todoJson['isCompleted'] as bool,
-        date:todoJson['date'] as String,
-        comments: todoJson['comments'] as String,
-        tags: List<String>.from(todoJson['tags'] as List<dynamic>),
+        isCompleted: (todoJson['is_completed'] as int) == 1 ? true : false, //sick ass cast
+        date:todoJson['due_date'] as String,
+        // comments: todoJson['comments'] as String,
+        // tags: List<String>.from(todoJson['tags'] as List<dynamic>),
+        comments: "",
+        tags: <String>[]
       );
     }).toList();
   } else {
